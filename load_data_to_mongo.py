3b60105fee5151614b8edb6d76ff2c694709269a
Mark Ewing <mewing@eastman.com>
2019-09-09 13:56:41 -0400

    improved reporting and other text index modes

--- load_data_to_mongo.py
+++ load_data_to_mongo.py
@@ -2,25 +2,46 @@ import pymongo
 import glob
 import json
 import re
+import nltk
 from tqdm import tqdm
 
 MONGO_CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
 DB = MONGO_CLIENT["ques_ans"]
-COL = DB["nasdaq"]
+COL = DB["nasdaq_ner_full_dbl"]
+COL.drop()
 
 
-def load_json_articles_to_db(collection, json_glob='data/articles/*.json'):
+def nltk_ner_extract(text, keep_all=True, join=False):
+    filtered_ner = ['ORGANIZATION', 'LOCATION', 'FACILITY', 'GPE']
+
+    chunks = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text)))
+    ne_list = []
+    for chunk in chunks:
+        if hasattr(chunk, 'label'):
+            if keep_all or chunk.label() in filtered_ner:
+                full_ne = ' '.join(c[0] for c in chunk)
+                ne_list.append(full_ne)
+
+    if join:
+        return ' '.join(ne_list)
+    else:
+        return ne_list
+
+
+def load_json_articles_to_db(collection, json_glob='data/articles/*.json', ner=True):
     for p in tqdm(glob.glob(json_glob)):
         with open(p, 'r') as f:
             article = json.load(f)
+            if ner:
+                article['ner'] = nltk_ner_extract(' '.join(article['text']))
             article['text'] = re.sub('   +', '\n', ''.join(article['text'])).split('\n')
             collection.insert_one(article)
 
 
-def create_index(collection):
-    collection.create_index([('text', pymongo.TEXT)], default_language='english')
+def create_index(collection, column):
+    collection.create_index(column, default_language='english')
 
 
 if __name__ == '__main__':
     load_json_articles_to_db(COL)
-    create_index(COL)
+    create_index(COL, [('ner', pymongo.TEXT), ('text', pymongo.TEXT)])
--- run_query_mongo.py
+++ run_query_mongo.py
@@ -1,9 +1,10 @@
 import pymongo
+import json
 from bert_squad import QABot
 
 MONGO_CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
 DB = MONGO_CLIENT["ques_ans"]
-COL = DB["nasdaq"]
+COL = DB["nasdaq_ner_full_dbl"]
 N_ANSWERS = 10
 
 qa_bot = QABot(download=False)
@@ -14,7 +15,7 @@ while True:
         break
 
     matches = COL.find({"$text": {"$search": question}},
-                        {"score": {"$meta": "textScore"}}) \
+                       {"score": {"$meta": "textScore"}}) \
         .sort([('score', {'$meta': 'textScore'})])\
         .limit(N_ANSWERS)
 
@@ -25,11 +26,25 @@ while True:
         full_text = ' '.join(doc['text'])
         answer = qa_bot.ask_question(full_text, question)
         try:
-            this_score = answer[2][0] * doc["score"]
+            this_score = answer[2][0]
+            if this_score > max_score:
+                max_score = this_score
+                best_answer = answer
+                best_doc = doc
         except TypeError:
             this_score = -999
-        if this_score > max_score:
-            max_score = this_score
-            best_answer = answer
-            best_doc = doc
-    print(f'\t* {best_answer[0][0]} (Confidence: {best_doc["score"]}, {answer[2]})')
+
+    full_text = ' '.join(best_doc['text'])
+    answer_start_idx = best_answer[1][0]
+    start = max([0, answer_start_idx - 20])
+    end = min([len(full_text), answer_start_idx + 20])
+    answer_context = '...' + full_text[start:end] + '...'
+
+    final_answer = {
+        'answer_text': best_answer[0][0],
+        'answer_start_idx': answer_start_idx,
+        'answer_context': answer_context,
+        'answer_confidence': best_answer[2][0],
+        'source_url': best_doc['url']
+    }
+    print(json.dumps(final_answer, indent=2))
